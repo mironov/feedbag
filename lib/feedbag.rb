666a4a001064f36c2bec781732b551c543ee8f01
Anton Mironov <ant.mironov@gmail.com>
2015-05-21 00:29:00 +0300

    skip ssl verification

--- lib/feedbag.rb
+++ lib/feedbag.rb
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -95,7 +95,7 @@ class Feedbag
     end
 
     begin
-      html = open(url) do |f|
+      html = open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE) do |f|
         content_type = f.content_type.downcase
         if content_type == "application/octet-stream" # open failed
           content_type = f.meta["content-type"].gsub(/;.*$/, '')
